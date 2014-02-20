04df57866d1c10f4053c7d62ed8d60712191ccbe
Gary Doran, Jr <garydoranjr@gmail.com>
2014-02-20 14:41:19 -0500

    add initial MATLAB code

---
+++ matlab/c_emd.c
@@ -0,0 +1,7 @@
+
+#include <mex.h>
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+    mexPrintf("Hello, world!\n");
+    return;
+}
---
+++ matlab/emd.m
@@ -0,0 +1,72 @@
+function dist = emd(X, Y, X_weights, Y_weights, distance, D)
+%Computes the Earth Mover's Distance between two weighted samples
+% emd(X, Y[, X_weights, Y_weights, distance, D])
+% X : First sample
+% Y : Second sample
+% X_weights : weights of elements in X (must sum to 1);
+%    default: 1/|X| is used as the weight of each element
+% Y_weights : weights of elements in Y (must sum to 1);
+%    default: 1/|Y| is used as the weight of each element
+% distance : valid distance type used by pdist2,
+%    or "precomputed" if the pairwise distances D is supplied;
+%    default: euclidean
+% D : precomputed distance matrix; ignored unless distance="precomputed";
+%    must be array of size |X|-by-|Y|
+if nargin < 2
+    error('Must at least provide two samples.');
+end
+if nargin < 3
+    X_weights = [];
+end
+if nargin < 4
+    Y_weights = [];
+end
+if nargin < 5
+    distance = 'euclidean';
+end
+if nargin < 6
+    D = [];
+end
+
+if ~strcmp(distance, 'precomputed')
+    n = length(X);
+    m = length(Y);
+    D = pdist2(X, Y, distance);
+    if isempty(X_weights)
+        X_weights = ones(1, n)/n;
+    elseif n ~= length(X_weights)
+        error('Size mismatch of X and X_weights');
+    else
+        X_weights = reshape(X_weights, 1, []);
+    end
+    if isempty(Y_weights)
+        Y_weights = ones(1, m)/m;
+    elseif m ~= length(Y_weights)
+        error('Size mismatch of Y and Y_weights');
+    else
+        Y_weights = reshape(Y_weights, 1, []);
+    end
+else
+    if isempty(D)
+        error('D must be supplied when distance=''precomputed''');
+    end
+    [n, m] = size(D);
+    if isempty(X_weights)
+        X_weights = ones(1, n)/n;
+    elseif n ~= length(X_weights)
+        error('Size mismatch of D and X_weights');
+    else
+        X_weights = reshape(X_weights, 1, []);
+    end
+    if isempty(Y_weights)
+        Y_weights = ones(1, m)/m;
+    elseif m ~= length(Y_weights)
+        error('Size mismatch of D and Y_weights');
+    else
+        Y_weights = reshape(Y_weights, 1, []);
+    end
+end
+
+dist = 1.0;
+%dist = _emd(X_weights, Y_weights, D);
+end
